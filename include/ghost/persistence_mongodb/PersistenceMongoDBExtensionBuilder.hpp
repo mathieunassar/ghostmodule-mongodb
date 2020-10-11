ab48ac1fb328ce38bb15e71f2ab79f1951bf2f56
MathieuNassar <mathieu.nassar@gmail.com>
2020-10-11 16:39:58 +0200

    ADD: module extension files

---
+++ include/ghost/persistence_mongodb/PersistenceMongoDBExtensionBuilder.hpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Mathieu Nassar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GHOST_PERSISTENCEMONGODBEXTENSIONBUILDER_HPP
+#define GHOST_PERSISTENCEMONGODBEXTENSIONBUILDER_HPP
+
+#include <ghost/module/ModuleExtensionBuilder.hpp>
+
+namespace ghost
+{
+class PersistenceMongoDBExtensionBuilder : public ghost::ModuleExtensionBuilder
+{
+public:
+};
+} // namespace ghost
+
+#endif // GHOST_PERSISTENCEMONGODBEXTENSIONBUILDER_HPP
---
+++ src/persistence_mongodb/PersistenceMongoDBExtension.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Mathieu Nassar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GHOST_INTERNAL_PERSISTENCEMONGODBEXTENSION_HPP
+#define GHOST_INTERNAL_PERSISTENCEMONGODBEXTENSION_HPP
+
+#include <ghost/persistence_mongodb/PersistenceMongoDBExtension.hpp>
+
+namespace ghost
+{
+namespace internal
+{
+class PersistenceMongoDBExtension : public ghost::PersistenceMongoDBExtension
+{
+public:
+	bool start() override;
+	void stop() override;
+	std::string getName() const override;
+};
+} // namespace internal
+} // namespace ghost
+
+#endif // GHOST_INTERNAL_PERSISTENCEMONGODBEXTENSION_HPP
